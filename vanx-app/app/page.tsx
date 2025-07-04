afa5a455cc3a6194e6addc1612f99b6c8f484be3
shion <shion48691811@gmail.com>
2025-07-04 19:17:05 +0900

    ボタンをサイズ変更、カスタマイズできるコンポーネント完成

--- vanx-app/app/page.tsx
+++ vanx-app/app/page.tsx
@@ -1,21 +1,25 @@
-"user client";
+"use client";
 
+import Image from 'next/image';
+import { Button } from "@/components/shared/Button";
 import { PostItem } from "@/components/post";
 import { FooterNavItem } from "@/components/shared";
 
-// 仮データの皆さん
+// 仮データ（ユーザー情報）
 const commonUser = {
   userId: "junpeichan@0310",
   userName: "じゅんぺいちゃん",
   imageSrc: "/icons/user-icon.svg",
 };
 
+// 仮データ（リアクション情報）
 const commonReaction = {
   reactionName: "平常心",
   reactionImageSrc: "/icons/reaction-icon.svg",
   category: "emoji" as const,
 };
 
+// 投稿一覧（仮データで5件分作成）
 const posts = Array.from({ length: 5 }, (_, i) => ({
   id: i + 1,
   ...commonUser,
@@ -44,13 +48,19 @@ export default function Home() {
                 <li key={post.id}>
                   <PostItem post={normalizedPost} />
                 </li>
-              )
+              );
             })}
           </ul>
         </div>
       </main>
 
       <FooterNavItem />
+      <Button
+        buttonType="redButton"
+        size="l"
+        text="登録"
+        className="shadow-top"
+      />
     </>
-  )
+  );
 }
--- vanx-app/app/test/page.tsx
+++ vanx-app/app/test/page.tsx
@@ -1,9 +1,11 @@
 import { PostingButton, PostItem, ReactionAddButton, ReactionBottomSheet } from "@/components/post";
+import { Button } from "@/components/shared/Button";
 
 export default function Test() {
   return (
     <main>
       <div className="absolute bottom-0">
+        <Button buttonType="redButton" size="l" text="登録" />
         <ReactionBottomSheet />
       </div>
     </main>
