3fde01a06b597317977f328dc21b62784f1363f0
shion <shion48691811@gmail.com>
2025-07-08 14:03:34 +0900

    モーダルの枠組みコンポーネント作成しました

---
+++ vanx-app/components/shared/Modal.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React, { useRef } from "react";
+
+export type ModalProps ={
+    openModal: boolean;
+    children: React.ReactNode;
+    onClose: () => void;
+}
+
+export function Modal({ openModal, onClose, children }: ModalProps) {
+    const ref = useRef<HTMLDivElement>(null);
+
+    // 背景クリックで閉じる
+    const OutClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (ref.current && !ref.current.contains(e.target as Node)) {
+        onClose();
+        }    };
+
+    if (!openModal) return null;
+    return (
+        <div className="bg-[#9A9A9A]/50 fixed inset-0 z-50 flex justify-center items-center" onClick={OutClick}>
+            <div 
+                className="bg-[#ffffff] rounded-2xl w-[350px] h-[250px] m-auto p-4"
+                ref={ref}
+                onClick={(e) => e.stopPropagation()}
+            >
+                {children}
+            </div>
+        </div>
+    );
+}
