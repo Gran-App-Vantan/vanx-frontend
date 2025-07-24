b42bc11426ef997fd888bdfd6b16579dc2423020
shion <shion48691811@gmail.com>
2025-07-24 13:36:58 +0900

    自分のプロフィールが上に表示されてスクロールされたらスタイルが少し変わるように作りました

--- vanx-app/app/test/page.tsx
+++ vanx-app/app/test/page.tsx
@@ -5,27 +5,33 @@ import { Button } from "@/components/shared";
 import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import Link from 'next/link';
-import { NumberOne, NumberTwo, Number } from "@/components/features/rankings/UsersProfile";
-
-
+import {  Number } from "@/components/features/rankings/UsersProfile";
+import { MyProfile } from "@/components/features/rankings/MyProfile";
+import { ReturnButton } from "@/components/shared/ReturnButton";
 
 export default function Test() {
     return (
-      <div className="flex flex-col gap-4">
-        {/* <NumberOne rank={1} name="じゅんPayん" image="/icon.png" score="100,000P" />
-        <NumberTwo rank={2} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" /> */}
-        <Number rank={1} name="じゅんPayん??" image="/icon.png" score="100,000P" />
-        <Number rank={2} name="paypay" image="/icon.png" score="50,000P" />
-        <Number rank={3} name="れおれおん" image="/icon.png" score="50,000P" />
-        <Number rank={4} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
-        <Number rank={5} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
-        <Number rank={6} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
-        <Number rank={7} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
-        <Number rank={8} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
-        <Number rank={9} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
-        <Number rank={10} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
-        <Number rank={11} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
-        <Number rank={12} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
-      </div>
+      <>
+        <div className="fixed top-0 w-full z-10">
+          <ReturnButton />
+          <div>
+              <MyProfile myImage="/icon.png" myName="じゅんPayん??" myRank={1} />
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 mt-50">
+          <Number rank={1} name="じゅんPayん??" image="/icon.png" score="100,000P" />
+          <Number rank={2} name="paypay" image="/icon.png" score="50,000P" />
+          <Number rank={3} name="れおれおん" image="/icon.png" score="50,000P" />
+          <Number rank={4} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
+          <Number rank={5} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
+          <Number rank={6} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
+          <Number rank={7} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
+          <Number rank={8} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
+          <Number rank={9} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
+          <Number rank={10} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
+          <Number rank={11} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
+          <Number rank={12} name="じゅんPayでーーーす" image="/icon.png" score="50,000P" />
+        </div>
+      </>
     )
-    }
+}
