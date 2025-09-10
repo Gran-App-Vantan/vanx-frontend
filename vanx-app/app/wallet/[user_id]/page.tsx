de77ec5e5c7de37651151b72035f3c3456e760a3
junpei-chan <jumpei24s1102270@gmail.com>
2025-09-10 19:34:28 +0900

    temporary commit

--- vanx-app/app/wallet/[user_id]/page.tsx
+++ vanx-app/app/wallet/[user_id]/page.tsx
@@ -1,7 +1,45 @@
-export default function PointDetail() {
+import Link from "next/link";
+import Image from "next/image";
+import { LeftArrowIcon } from "@/components/shared/icons";
+
+export default function Wallet() {
   return (
-    <>
-      {/* ポイント詳細ページ */}
-    </>
+    <main>
+      <div>
+        <Link href="/">
+          <LeftArrowIcon  color="black"/>
+          戻る
+        </Link>
+        <div>
+          <Image 
+            src="/icons/default-user-icon.svg"
+            alt="default-user-icon"
+            width={50}
+            height={50}
+          />
+          <p>じゅんぺいちゃん</p> {/* name */}
+        </div>
+        <div>
+          <p>現在のポイント</p>
+          <p>1,000P</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <button>すべて</button>
+          </div>
+          <div>
+            <button>獲得ポイント</button>
+          </div>
+          <div>
+            <button>損失ポイント</button>
+          </div>
+        </div>
+        <ul>
+          
+        </ul>
+      </div>
+    </main>
   );
 }
