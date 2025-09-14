fd0e54aae8ad5021fed35e78b1cf01f5c6e5c0c8
junpei-chan <jumpei24s1102270@gmail.com>
2025-09-14 16:54:32 +0900

    add: PointLogItemProps

--- vanx-app/components/features/wallet/PointLogItem.tsx
+++ vanx-app/components/features/wallet/PointLogItem.tsx
@@ -1,8 +1,13 @@
-"use client";
+export type PointLogItemProps = {
+    time: string;
+    boothName: string;
+    point: number;
+    isPuls: boolean;
+}
 
 export  function PointLogItem({isPulse}: {isPulse: boolean}) {
     return (
-        <div className="flex justify-center items-center gap-4 px-2 py-5 border-b-[0.5px] border-b-text-gray w-[408px]  font-medium bg-white text-normal"> 
+        <div className="flex justify-center items-center gap-4 px-2 py-5 border-b-[0.5px] border-b-text-gray w-[408px] font-medium bg-white text-normal"> 
             <p className="text-label">6/5 10:01</p>
             <p>インディアンポーカー</p>
             <p className={`${isPulse ? "text-green-letters" : "text-red-letters"}`}>{isPulse ?"+":"-"}8000</p>
