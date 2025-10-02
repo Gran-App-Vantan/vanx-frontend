7ddf96e34f0ef72be5b2eacb4ed5b949e2de088d
junpei-chan <jumpei24s1102270@gmail.com>
2025-10-02 19:47:45 +0900

    create: usePostDelete

--- vanx-app/app/page.tsx
+++ vanx-app/app/page.tsx
@@ -1,34 +1,18 @@
-// app/page.tsx
 "use client";
 
-import { useRouter } from "next/navigation";
 import { PostList } from "@/components/features/post";
 import { Header, FooterNavItem } from "@/components/shared/";
-import { PostDelete } from "@/api/post/";
 import { useUser } from "@/contexts/UserContext";
 import { usePosts } from "@/hooks/usePosts";
+import { usePostDelete } from "@/hooks/usePostDelete";
 
 export default function Home() {
-  const router = useRouter();
   const { user } = useUser();
   const { posts, setPosts, loading, error } = usePosts();
+  const { handlePostDelete } = usePostDelete();
 
-  const handlePostDelete = async (postId: number) => {
-    try {
-      const response = await PostDelete({ postId });
-      
-      if (response.success) {
-        setPosts((prevPosts) => prevPosts.filter((post) => post.id !== postId));
-        alert("投稿を削除しました");
-        router.refresh();
-      } else {
-        console.error("削除に失敗しました:", response.message);
-        alert("削除に失敗しました");
-      }
-    } catch (err) {
-      console.error("削除エラー:", err);
-      alert("削除処理中にエラーが発生しました");
-    }
+  const onPostDelete = async (postId: number) => {
+    await handlePostDelete(postId, setPosts);
   };
 
   if (loading) return <div>読み込み中...</div>;
@@ -39,8 +23,7 @@ export default function Home() {
       <Header />
       <main>
         <div className="mt-24 mb-20">
-          {/* UI制御はPostListに任せる */}
-          <PostList posts={posts} onPostDelete={handlePostDelete} />
+          <PostList posts={posts} onPostDelete={onPostDelete} />
         </div>
       </main>
       <FooterNavItem userId={user?.id} />
