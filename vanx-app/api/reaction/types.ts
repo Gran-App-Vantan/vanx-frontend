d2bbcdbf7f07eeb51f1231054f33dd1fad4e20d7
junpei-chan <jumpei24s1102270@gmail.com>
2025-10-17 15:40:27 +0900

    add: ReactionData type

--- vanx-app/api/reaction/types.ts
+++ vanx-app/api/reaction/types.ts
@@ -1,3 +1,14 @@
+import { Reaction } from "@/api/post/";
+
+export type ReactionData = {
+  data: Reaction[];
+  currentPage: number;
+  lastPage: number;
+  nextPageUrl: string | null;
+  prevPageUrl: string | null;
+  total: number;
+}
+
 export type UseReactionsOptions = {
   category: "all" | "face" | "nature" | "food" | "activity" | "travel" | "object" | "symbol" | "original";
   page: number;
