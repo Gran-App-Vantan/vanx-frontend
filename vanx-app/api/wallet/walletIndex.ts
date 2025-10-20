da55d239db316fa2c9fc5831aeb3a7fdee65d6cc
junpei-chan <jumpei24s1102270@gmail.com>
2025-10-20 19:55:01 +0900

    add: walletIndex.ts

---
+++ vanx-app/api/wallet/walletIndex.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+import humps from "humps";
+
+export type WalletIndexResponse = 
+  | {
+    success: true;
+    message: string;
+    data: {
+      pointBalance: number;
+    }
+  }
+  | {
+    success: false;
+    message: string;
+    errors: {
+      err: string;
+    }
+  }
+
+export async function WalletIndex(): Promise<WalletIndexResponse> {
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/account/wallet`;
+  const authToken = Cookies.get("authToken");
+
+  return axios
+    .get<WalletIndexResponse>(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        Accept: "application/json"
+      }
+    })
+    .then(res => {
+      res.data = humps.camelizeKeys(res.data) as typeof res.data;
+      return res.data;
+    })
+    .catch(err => {
+      console.error("ERROR", err);
+
+      return {
+        success: false,
+        message: "ウォレット情報の取得に失敗しました。",
+        errors: { err: err.message }
+      };
+    })
+}
